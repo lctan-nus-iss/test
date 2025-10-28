915ca7f02a56ac7dd1362e6ae1048f47e64cdc10
Tan Lee Chiang <lctan@nus.edu.sg>
2025-10-28 16:43:50 +0800

    Remove parameterized test for addition

--- test_calculator.py
+++ test_calculator.py
@@ -8,12 +8,3 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 5) == -5
     assert subtract(-1, -1) == 0
-''
-@pytest.mark.parametrize("a,b,expected", [
-    (2, 3, 5),
-    (-1, 1, 0),
-    (100, 200, 300)
-])
-def test_add_param(a, b, expected):
-    assert add(a, b) == expected
-''
